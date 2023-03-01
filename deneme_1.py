b472462928405ec2510452590caa674515bdaafd
mustafaaskin <mustafaaskin@hotmail.com>
2023-03-01 12:44:09 +0300

    Add files via upload

---
+++ deneme_1.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+print("it started.")
+
+df = pd.read_excel("TCMB_VERI_1.xlsx")
+
+print(df)
+
+print(df[['UK_KOD','2009M3']])
+
+
+A=5
+print(A)
+
+print("Hello")
---
+++ deneme_3.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+df = pd.read_excel("TCMB_VERI_1.xlsx")
+
+print(df)
+
+print(df[['UK_KOD','2009M3']])
+
+
+A=5
+print(A)
+
+print("Hello")
